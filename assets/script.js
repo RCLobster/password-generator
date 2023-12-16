715876ac8dd77f24e1eb028177b48c4dcfb44f4b
RCLobster <44736025+RCLobster@users.noreply.github.com>
2023-12-16 09:42:52 -0800

    added psuedo code plan

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
+/* The Plan
+1. Click generate password button to initiate prompts to user about password criteria
+2. Allow user to select which criteria are included in the password using alert()
+3. Prompt user to choose password length btwn 8-128
+4. Prompt user to choose which characters to include lowercase, uppercase, numeric, special characters 
+   (can be any combination of charcters)
+5. All inputs should be validated and a minimum of one criteria should be selected, if none are selected restart
+6. When all prompts are complete and at least one is chosen, generate a password which matches the criteria
+7. Display password on website
+*/
+
 // Assignment code here
 
 
