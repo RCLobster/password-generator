55a8b19290349e6d0705573adae1cf9cee382c0d
RCLobster <44736025+RCLobster@users.noreply.github.com>
2023-12-16 10:11:31 -0800

    Added logic to check if user input length is between 8 and 128

--- assets/script.js
+++ assets/script.js
@@ -25,10 +25,21 @@ function writePassword() {
 }
 
 function generatePassword() {
-  alert("Let's make you a password. Choose which of the following criteria this password should meet:");
+  //Create a variable to store user input about password length
   var length = prompt("How long should your password be? Select a number between 8 and 128.");
   length = Number(length);
-  
+  //if the length is not between 8 and 128 make the user try again
+  if (length < 8 || length > 128) {
+    alert("Sorry that number is not between 8 and 128. Please try again.");
+    return;
+  }
+  console.log("Password length: " + length);
+
+  //Create variables to store user input about types of characters to include
+  var hasLowercase = window.confirm("Would you like the password to contain lowercase letters?");
+  console.log("Lowercase: " + hasLowercase)
+  var hasUppercase = window.confirm("Would you like the password to contain uppercase letters?");
+  console.log("Uppercase: " + hasUppercase);
 }
 
 // Add event listener to generate button
